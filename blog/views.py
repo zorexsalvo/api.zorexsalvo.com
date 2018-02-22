2c3fd2c323ab13755a662520967d79ae8cc7dcdb
Zorex Salvo <zorexsalvo@gmail.com>
2018-02-22 17:43:09 +0800

    Order the post by date_created desc

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,6 @@ from .serializers import PostSerializer
 class PostViewSet(viewsets.ReadOnlyModelViewSet):
     """A viewset for retrieving posts."""
 
-    queryset = Post.objects.all()
+    queryset = Post.objects.all().order_by('-date_created')
     serializer_class = PostSerializer
     lookup_field = 'slug'
